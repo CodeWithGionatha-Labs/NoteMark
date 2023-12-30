bd04c9706ad412158211c2727686c3f95ad5d5d2
gionny96 <gionathas@gmail.com>
2023-12-30 15:44:01 +0100

    Add FloatingNoteTitle component

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,6 +2,7 @@ import {
   ActionButtonsRow,
   Content,
   DraggableTopBar,
+  FloatingNoteTitle,
   MarkdownEditor,
   NotePreviewList,
   RootLayout,
@@ -18,6 +19,7 @@ const App = () => {
           <NotePreviewList className="mt-3 space-y-1" />
         </Sidebar>
         <Content className="border-l bg-zinc-900/50 border-l-white/20">
+          <FloatingNoteTitle className="pt-2" />
           <MarkdownEditor />
         </Content>
       </RootLayout>
---
+++ src/renderer/src/components/FloatingNoteTitle.tsx
@@ -0,0 +1,12 @@
+import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export const FloatingNoteTitle = ({ className, ...props }: ComponentProps<'div'>) => {
+  const title = 'Note Title'
+
+  return (
+    <div className={twMerge('flex justify-center', className)} {...props}>
+      <span className="text-gray-400">{title}</span>
+    </div>
+  )
+}
